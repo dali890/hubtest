c26b98691ce0e7c2e894d9eb7be226548b1bed71
dali890 <mohamedali.benelbekri90@gmail.com>
2015-11-30 12:21:21 +0100

    first commit

---
+++ src/main/java/app_exemple/exemple/App.java
@@ -0,0 +1,13 @@
+package app_exemple.exemple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
